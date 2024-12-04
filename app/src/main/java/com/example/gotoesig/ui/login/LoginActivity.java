fae08effce9eea79307ad14ec7e0df9ee2099b4d
Nasim Benyacoub <nasimbenyacoub97@gmail.com>
2024-12-04 15:09:22 +0100

    login UI done

---
+++ app/src/main/java/com/example/gotoesig/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.gotoesig.ui.login;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.gotoesig.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Configura el comportamiento del login aquí
+    }
+}
